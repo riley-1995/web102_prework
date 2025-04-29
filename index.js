2b44d9a77d1b4be7fea96740a8ce1d7bdcb840c0
riley-1995 <rileydenn@gmail.com>
2025-04-29 12:02:54 -0700

    Update index.js to display top game and runner up.

--- index.js
+++ index.js
@@ -188,7 +188,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [first, second, ...others] = sortedGames
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topGame = document.createElement('p')
+topGame.innerHTML = `${first.name}`
+firstGameContainer.appendChild(topGame)
 
 // do the same for the runner up item
+const secondGame = document.createElement('p')
+secondGame.innerHTML = `${second.name}`
+secondGameContainer.appendChild(secondGame)
