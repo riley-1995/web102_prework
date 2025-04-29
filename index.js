b0486479c59736e845a7630dcd2226d0ab765562
riley-1995 <rileydenn@gmail.com>
2025-04-29 06:16:46 -0700

    Update index.js to enable filtering games by funded, unfunded, or all.

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ function addGamesToPage(games) {
         <h4>${game.name}</h4>
         <p class='game-description'>${game.description}</p>
         <div class='game-stats'>
-            <p>Pledged: ${game.pledged}</p>
-            <p>Goal: ${game.goal}</p>
-            <p>Backers: ${game.backers}</p>
+            <p>Pledged: ${game.pledged.toLocaleString()}</p>
+            <p>Goal: ${game.goal.toLocaleString()}</p>
+            <p>Backers: ${game.backers.toLocaleString()}</p>
         </div>
         `
 
@@ -106,10 +106,10 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let unfunded = GAMES_JSON.filter(game => {return game.pledged < game.goal});
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(unfunded);
 }
 
 // show only games that are fully funded
@@ -117,10 +117,10 @@ function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let funded = GAMES_JSON.filter(game => {return game.pledged >= game.goal});
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(funded);
 }
 
 // show all games
@@ -128,6 +128,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON)
 
 }
 
@@ -137,6 +138,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener('click', filterUnfundedOnly);
+fundedBtn.addEventListener('click', filterFundedOnly);
+allBtn.addEventListener('click', showAllGames);
 
 
 /*************************************************************************************
